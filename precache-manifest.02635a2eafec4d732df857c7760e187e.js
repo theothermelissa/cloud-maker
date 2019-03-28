8b8b48914519666b230cc5b469cd11d90a734b98
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-03-28 13:58:57 -0500

    Updates

---
+++ precache-manifest.02635a2eafec4d732df857c7760e187e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d31465e673b83c8765b7952af02cd6a3",
+    "url": "/static/media/smoke.d31465e6.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d38c07678a162fa533b6",
+    "url": "/static/js/main.0b465aff.chunk.js"
+  },
+  {
+    "revision": "f178e7cc11fd2b59b9e4",
+    "url": "/static/js/2.b7dbac30.chunk.js"
+  },
+  {
+    "revision": "d38c07678a162fa533b6",
+    "url": "/static/css/main.0685a370.chunk.css"
+  },
+  {
+    "revision": "f178e7cc11fd2b59b9e4",
+    "url": "/static/css/2.3fdcddeb.chunk.css"
+  },
+  {
+    "revision": "8f25da34a28d5628430c3ccf4e3f28a0",
+    "url": "/index.html"
+  }
+];
--- src/components/ColorButtons/ColorsForButtons.js
+++
@@ -1,9 +0,0 @@
-import { mysky, myroyal, mycharcoal, mynavy, mybluegray } from '../Colors';
-
-export default {
-  sky: mysky,
-  bluegray: mybluegray,
-  royal: myroyal,
-  charcoal: mycharcoal,
-  navy: mynavy,
-}
